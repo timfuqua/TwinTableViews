ffce5942a7e7a7aa6882854a4e34115299f6ac9e
Tim Fuqua <timfuqua@gmail.com>
2015-02-19 17:14:19 -0800

    Initial commit with empty project.

---
+++ TwinTableViews/TwinTableViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwinTableViews
+//
+//  Created by Tim Fuqua on 2/19/15.
+//  Copyright (c) 2015 FuquaProductions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
